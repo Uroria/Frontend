ca51f435cd6cc924058127c2c3dcf50d75dd1b4e
nicosammito <kynxyd-bUxnek-9vohsi>
2022-05-21 14:14:35 +0200

    added header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
-import type { NextPage } from 'next'
-import Button from "../components/Button";
+import type {NextPage} from 'next'
+import UroriaNavbar from "../components/Navigation";
+import UroriaHeader from "../components/UroriaHeader";
+import AvatarList from "../components/AvatarList";
+import Avatar from "../components/Avatar";
+import {IconUsers} from "@tabler/icons";
+import IconButton from "../components/IconButton";
+import AttentionText from "../components/AttentionText";
+import StatusView from "../components/StatusView";
 
 const Home: NextPage = () => {
-  return <>
-    <Button type={"green"} text={"Jetzt Uroria.com joinen!"}/>
-    <Button type={"blue"} text={"Jetzt Uroria.com joinen!"}/>
-    <Button type={"red"} text={"Jetzt Uroria.com joinen!"}/>
+    return <div style={{overflowX: "hidden"}}>
+        <UroriaNavbar/>
+        <UroriaHeader>
+            <AvatarList>
+                <Avatar image={<img src={"/ururiabot.png"}/>}/>
+                <Avatar image={<img src={"/jan.png"}/>}/>
+                <Avatar image={<img src={"/gecoded.png"}/>}/>
+                <Avatar image={<img src={"/paul.png"}/>}/>
+                <IconButton><IconUsers size={20}/></IconButton>
+            </AvatarList>
+            <AttentionText top={["5px", "0px", "10px", "10px", "10px", "-50px"]} left={["380px", "370px", "450px", "570px", "650px", "770px"]}>Jetzt bei Uroria<br/>bewerben!</AttentionText>
+            <h1>Der beste MMO-RPG Server in <br/>Deutschland: Uroria.com</h1>
 
-  </>
+            <StatusView headline={"Netzwerk Status"} icon={"/haken.png"}>Active</StatusView>
+            <StatusView headline={"Nächstes Event"}>6ter September, 10 Uhr</StatusView>
+            <StatusView headline={"Gewinn"} icon={"/coins.png"}>150</StatusView>
+
+        </UroriaHeader>
+    </div>
 }
 
 export default Home
