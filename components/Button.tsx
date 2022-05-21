a0b1e97c3c690e6ce2ad4659f31cdb88370be028
nicosammito <kynxyd-bUxnek-9vohsi>
2022-05-21 14:14:03 +0200

    adding detailed props definitions

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,13 @@
-const Button = (props: any) => {
-    return <button className={"btn-uroria " + "btn-" + props.type}>
-            {props.text}
-        </button>
+import {CSSProperties, FunctionComponent, ReactNode} from "react";
+
+type ButtonProps = {
+    color: "green" | "blue" | "red"
+    text: ReactNode | string,
+    style?: CSSProperties | undefined
 }
+
+const Button: FunctionComponent<ButtonProps> = ({text, color,style}) => <button style={style} className={"btn-uroria " + "btn-" + color}>
+    {text}
+</button>
 
 export default Button;
