90e66f379f73af3de0c85ffe7a742ecff6e3a78d
nicosammito <kynxyd-bUxnek-9vohsi>
2022-05-21 14:07:29 +0200

    including bootstrap in main component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+{/* The following line can be included in your src/index.js or App.js file*/}
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/uroria.scss'
 import type { AppProps } from 'next/app'
 
