cc7e664a1e8d3ba955a24e38878b488c1b65e464
nicosammito <kynxyd-bUxnek-9vohsi>
2022-05-30 18:06:23 +0200

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['de_DE', 'en_EN'],
+    defaultLocale: 'en_EN',
+  },
 }
 
 module.exports = nextConfig
