446f5db062d5b18c074fea7de8f01e5db5edaa19
nicosammito <kynxyd-bUxnek-9vohsi>
2022-06-20 19:39:13 +0200

    added disable option for buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,10 +3,11 @@ import {CSSProperties, FunctionComponent, ReactNode} from "react";
 type ButtonProps = {
     color: "green" | "blue" | "red"
     text: ReactNode | string,
-    style?: CSSProperties | undefined
+    style?: CSSProperties | undefined,
+    disabled?: boolean
 }
 
-const Button: FunctionComponent<ButtonProps> = ({text, color,style}) => <button style={style} className={"btn-uroria " + "btn-" + color}>
+const Button: FunctionComponent<ButtonProps> = ({text, color,style, disabled = false}) => <button style={style} className={"btn-uroria " + "btn-" + color + " " + (disabled ? "disabled" : "")}>
     {text}
 </button>
 
