6e5b9cde123b61d4c74ff49ad8bccc3a01b86999
nicosammito <kynxyd-bUxnek-9vohsi>
2022-06-20 21:48:36 +0200

    reduced getlanguage method to seralize language file

--- src/utils/Language.ts
+++ src/utils/Language.ts
@@ -1,9 +1,5 @@
 import propertiesReader from 'properties-reader';
-import {IUseLanguage} from "../@types/TLanguage";
 
-export const useLanguage = async (locale: string | undefined): Promise<IUseLanguage>  => {
-    const languageProperties = await propertiesReader(__dirname + "/../../../public/Language/Language_" + locale + ".properties").getAllProperties();
-    const languageName = locale;
-    return {languageName, languageProperties}
-}
+export const getLanguage = async (locale: string | undefined): Promise<{ [key: string]: propertiesReader.Value }> =>
+    propertiesReader(__dirname + "/../../../public/Language/Language_" + locale + ".properties").getAllProperties();
 
