349b42ad02a53599f36fa8fa006f269e01c941f2
nicosammito <kynxyd-bUxnek-9vohsi>
2022-06-20 21:51:06 +0200

    types

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,16 +1,17 @@
 import {CSSProperties, FunctionComponent, ReactNode} from "react";
-import {Container} from "react-bootstrap";
 
 type SectionProps = {
     children: ReactNode,
     useContainer?: boolean,
     useTransition?: boolean
-    title?: string
     src?: string,
     paddingY?: number
+    transitionOnlyTop?: boolean,
+    className?: string,
+    negativeMarginTop?: boolean
 }
 
-const Section: FunctionComponent<SectionProps> = ({children, src, useContainer = true, title, useTransition = true, paddingY}) => {
+const Section: FunctionComponent<SectionProps> = ({children, src, useContainer = true, useTransition = true, paddingY= 0, transitionOnlyTop = false, className, negativeMarginTop = false}) => {
 
     const style: CSSProperties = {
         background: "linear-gradient(315deg, rgba(0, 0, 0, 0.80) 0, rgba(0, 0, 0, 0.80) 100%), url('" + src + "') center",
