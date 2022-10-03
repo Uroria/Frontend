544246b1456ee0a6fa4be7b54a348c09413db920
nicosammito <kynxyd-bUxnek-9vohsi>
2022-10-04 01:52:20 +0200

    added Input to CharacterView and added onBlur functionallity to grab username and replace texture

--- src/sections/home/CosmeticSection.tsx
+++ src/sections/home/CosmeticSection.tsx
@@ -7,6 +7,7 @@ import CosmeticPreview from "../../components/CosmeticPreview";
 import {JsonToGltf} from "../../utils/JsonToGltf";
 import {fetcher} from "../../utils/Fetcher";
 import CharacterPreview from "../../components/CharacterPreview";
+import Input from "../../components/Input";
 
 const allHats: CosmeticsProps[] = [{
     gltf: "Witch_Hat",
@@ -32,12 +33,16 @@ const allHats: CosmeticsProps[] = [{
 const CosmeticSection = () => {
 
     const [currentHat, setCurrentHat] = useState(allHats[0]);
+    const [currentSkinName, setCuurentSkinName] = useState("lvckyfelix")
     const {data, error} = useSWR('/assets/3dModels/player.json', fetcher)
 
     return <Section>
         <Row>
-            <Col lg={"5"}>
-                <CharacterPreview skinGltf={JsonToGltf(data)} skinName={"lvckyfelix"} hatModel={currentHat}/>
+            <Col lg={"5"} className={"position-relative"}>
+                <CharacterPreview skinGltf={JsonToGltf(data)} skinName={currentSkinName} hatModel={currentHat}/>
+                <div style={{position: "absolute", bottom: ".5rem", left: "50%", transform: "translateX(-50%)"}}>
+                    <Input aria-describedby={"*leave input to enter unsername"} onBlur={event => event.target.value && event.target.value != "" ? setCuurentSkinName(event.target.value.toLocaleLowerCase()) : undefined} width={300} placeholder={"username"}/>
+                </div>
             </Col>
             <Col lg={"7"}>
                 <h1>Gestalte deinen <span className="mark">Charakter,</span></h1><br/>
@@ -51,7 +56,8 @@ const CosmeticSection = () => {
                 <Row>
                     {
                         allHats.map((hat, index) => {
-                            return <Col xs={4} sm={4} md={3} lg={3} xxl={2} onClick={() => setCurrentHat(allHats[index])}>
+                            return <Col xs={4} sm={4} md={3} lg={3} xxl={2}
+                                        onClick={() => setCurrentHat(allHats[index])}>
                                 <CosmeticPreview hatModel={{gltf: hat.gltf}} rareness={"Legendeary"}/>
                             </Col>
                         })
