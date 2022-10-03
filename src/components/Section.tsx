5dbbb50c876a31f16fec8ee8781cd3d092d91a97
nicosammito <kynxyd-bUxnek-9vohsi>
2022-10-03 15:53:18 +0200

    new section component with clip-path background

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -9,17 +9,7 @@ type SectionProps = {
 const Section: FunctionComponent<SectionProps> = ({children}) => <div className={styles.section}>
     <Container className={styles.section__container}>
         {children}
-        {!transitionOnlyTop ? <div className={"transition"}/> : null}
-    </> : null;
-
-
-    return <>
-        {useContainer ?
-            <div className={"section" + (className ? " " + className : "")} style={style}><div className={"container"}>{children}</div> </div> :
-            <div className={"section" + (className ? " " + className : "")} style={style} children={children}/>}
-    </>
-
-}
-
+    </Container>
+</div>
 
 export default Section;
