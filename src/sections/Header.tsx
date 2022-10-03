65429556e144086de31bff24a6f64156c24afba4
nicosammito <kynxyd-bUxnek-9vohsi>
2022-10-03 15:57:03 +0200

    new Header Section

---
+++ src/sections/Header.tsx
@@ -0,0 +1,41 @@
+import styles from '../../styles/components/Header.module.scss'
+import {Container} from "react-bootstrap";
+import Button from "../components/Button";
+import {
+    IconBrandDiscord,
+    IconBrandInstagram,
+    IconBrandYoutube,
+    IconChevronDown,
+    IconDeviceGamepad2
+} from "@tabler/icons";
+
+const Header = () => <div className={styles.header}>
+    <Container className={styles.header__container}>
+        <div className={styles.header__middle}>
+            <h1>Spiele <span className="mark">kostenlos,</span></h1><br/>
+            <h1>mit deiner <span className="mark">Community!</span></h1><br/>
+            <p>Mit dem Ziel, sich als einen starken Event-Hoster in der Minecraft-Szene zu etablieren, bietet dir Uroria
+                die einzigartige Möglichkeit, Server nach deinen Vorstellungen, an dich und deine Community anzupassen.
+                Wir haben uns es zu unserer Aufgabe gemacht, unter der Verwendung eines eigen Texturenpakets die
+                Spielfähigkeit und Abwechslung auf unserem Server zu erhöhen, damit erschaffen wir Systeme, die noch
+                über Minecraft selbst hinaus gehen.</p>
+            <Button size={"lg"} type={"square"}><IconDeviceGamepad2
+                style={{marginRight: "0.5rem", rotate: "-10deg"}}/>Play now!</Button>
+            <Button size={"lg"} type={"square"}><IconBrandDiscord style={{marginRight: "0.5rem", rotate: "-10deg"}}/>Discord</Button>
+        </div>
+
+        <div className={styles.header__bottom}>
+            <div className={styles.header__bottom_socialmedia}>
+                <Button size={"sm"} type={"round"} icon={true}><IconBrandDiscord/></Button>
+                <Button size={"sm"} type={"round"} icon={true}><IconBrandYoutube/></Button>
+                <Button size={"sm"} type={"round"} icon={true}><IconBrandInstagram/></Button>
+            </div>
+            <div className={styles.header__bottom_moreinfos}>
+                <Button size={"md"} type={"round"} color={"white"}><IconChevronDown style={{marginRight: "0.5rem"}}/>More
+                    Infos</Button>
+            </div>
+        </div>
+    </Container>
+</div>
+
+export default Header;
