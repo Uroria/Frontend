6f690277caceade5b4233a3424fdd0b1c9c1eee6
nicosammito <kynxyd-bUxnek-9vohsi>
2022-10-03 15:37:26 +0200

    new button for rounded and squared buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,18 @@
-import {CSSProperties, FunctionComponent, ReactNode} from "react";
+import {FunctionComponent, ReactNode} from "react";
+import styles from '../../styles/components/Button.module.scss'
 
 type ButtonProps = {
-    color: "green" | "blue" | "red"
-    text: ReactNode | string,
-    style?: CSSProperties | undefined,
-    disabled?: boolean
+    href?: string,
+    size?: "lg" | "md" | "sm",
+    color?: "white" | "black" | "blue",
+    type?: "round" | "square",
+    icon?: boolean,
+    children: ReactNode,
 }
 
-const Button: FunctionComponent<ButtonProps> = ({text, color,style, disabled = false}) => <button style={style} className={"btn-uroria " + "btn-" + color + " " + (disabled ? "disabled" : "")}>
-    {text}
-</button>
+const Button: FunctionComponent<ButtonProps> = ({href, children, size = "md", color = "black", type = "round", icon = false}) => <a
+    href={href} className={(styles["btn-" + color + "-" + type]) + " " + styles["btn-" + size] + " " + (icon ? styles["btn_icon"] : undefined)}>
+    {children}
+</a>
 
 export default Button;
