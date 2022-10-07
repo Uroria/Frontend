011ca924e660d4c9934936f41eb3fbe90f602d54
nicosammito <kynxyd-bUxnek-9vohsi>
2022-10-07 02:30:45 +0200

    removed unnecessary props and added extionsion for interface to allow many more props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,17 @@
-import {FunctionComponent, ReactNode} from "react";
+import {DetailedHTMLProps, FunctionComponent, InputHTMLAttributes, ReactNode} from "react";
 import styles from '../../styles/components/Button.module.scss'
 
-type ButtonProps = {
+interface ButtonProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
     href?: string,
-    size?: "lg" | "md" | "sm",
+    buttonSize?: "lg" | "md" | "sm",
     color?: "white" | "black" | "blue",
     type?: "round" | "square",
     icon?: boolean,
     children: ReactNode,
 }
 
-const Button: FunctionComponent<ButtonProps> = ({href, children, size = "md", color = "black", type = "round", icon = false}) => <a
-    href={href} className={(styles["btn-" + color + "-" + type]) + " " + styles["btn-" + size] + " " + (icon ? styles["btn_icon"] : undefined)}>
+const Button: FunctionComponent<ButtonProps> = ({children, buttonSize = "md", color = "black", type = "round", icon = false, ...args}) =>
+    <a className={(styles["btn-" + color + "-" + type]) + " " + styles["btn-" + buttonSize] + " " + (icon ? styles["btn-icon"] : "")} {...args}>
     {children}
 </a>
 
