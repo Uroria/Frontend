3246cc3540f265e98f6959056e34808421fdf428
nicosammito <kynxyd-bUxnek-9vohsi>
2022-10-07 02:32:48 +0200

    added Tab component

--- src/sections/home/CosmeticSection.tsx
+++ src/sections/home/CosmeticSection.tsx
@@ -3,11 +3,12 @@ import {Col, Row} from "react-bootstrap";
 import {useState} from "react";
 import {CosmeticsProps} from "../../components/Minecraft3DCharacter";
 import useSWR from 'swr'
-import CosmeticPreview from "../../components/CosmeticPreview";
 import {JsonToGltf} from "../../utils/JsonToGltf";
 import {fetcher} from "../../utils/Fetcher";
 import CharacterPreview from "../../components/CharacterPreview";
 import Input from "../../components/Input";
+import {Tab, Tabs} from "../../components/Tabs";
+import CosmeticPreview from "../../components/CosmeticPreview";
 
 const allHats: CosmeticsProps[] = [{
     gltf: "Witch_Hat",
@@ -34,14 +35,16 @@ const CosmeticSection = () => {
 
     const [currentHat, setCurrentHat] = useState(allHats[0]);
     const [currentSkinName, setCuurentSkinName] = useState("lvckyfelix")
-    const {data, error} = useSWR('/assets/3dModels/player.json', fetcher)
+    const {data, error} = useSWR('./assets/3dModels/player.json', fetcher)
 
     return <Section>
         <Row>
             <Col lg={"5"} className={"position-relative"}>
                 <CharacterPreview skinGltf={JsonToGltf(data)} skinName={currentSkinName} hatModel={currentHat}/>
-                <div style={{position: "absolute", bottom: ".5rem", left: "50%", transform: "translateX(-50%)"}}>
-                    <Input blink={true} description={"leave input to enter unsername"} onBlur={event => event.target.value && event.target.value != "" ? setCuurentSkinName(event.target.value.toLocaleLowerCase()) : undefined} width={300} placeholder={"username"}/>
+                <div style={{position: "absolute", bottom: "0", left: "50%", transform: "translateX(-50%)"}}>
+                    <Input blink={true} description={"leave input to enter unsername and skin viewer is currently only available for Steve 1.7+ skins"}
+                           onBlur={event => event.target.value && event.target.value != "" ? setCuurentSkinName(event.target.value.toLocaleLowerCase()) : undefined}
+                           width={300} placeholder={"username"}/>
                 </div>
             </Col>
             <Col lg={"7"}>
@@ -53,16 +56,28 @@ const CosmeticSection = () => {
                     neuen Items über Modelle bis neue Rüstungen hinzufügen können, sind kosmetische Gegenstände ein
                     zentraler Bestandteil. So können alle Spieler dieses Servers ihren Spielcharakter zusätzlich
                     verschönern!</p>
-                <Row>
-                    {
-                        allHats.map((hat, index) => {
-                            return <Col xs={4} sm={4} md={3} lg={3} xxl={2}
-                                        onClick={() => setCurrentHat(allHats[index])}>
-                                <CosmeticPreview hatModel={{gltf: hat.gltf}} rareness={"Legendeary"}/>
-                            </Col>
-                        })
-                    }
-                </Row>
+
+
+                <Tabs>
+                    <Tab title="🤠 Hat Cosmetics">
+                        <Row>
+                            {
+                                allHats.map((hat, index) => {
+                                    return <Col xs={4} sm={4} md={3} lg={3} xxl={2}
+                                                onClick={() => currentHat != allHats[index] ? setCurrentHat(allHats[index]) : null}>
+                                        <CosmeticPreview hatModel={{gltf: hat.gltf}} rareness={"Legendeary"}/>
+                                    </Col>
+                                })
+                            }
+                        </Row>
+                    </Tab>
+                    <Tab title="🎒 Backpack Cosmetics">
+                        <p>Backpack Cosmetics coming soon</p>
+                    </Tab>
+                    <Tab title="🎈 Balloon Cosmetics">
+                        <p>Balloon Cosmetics coming soon</p>
+                    </Tab>
+                </Tabs>
             </Col>
         </Row>
     </Section>
