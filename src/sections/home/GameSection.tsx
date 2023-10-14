0113c054f92a64760d369f7997d202ed5a0246a2
nicosammito <nico@sammito.de>
2023-10-14 14:17:00 +0200

    removed name for game section, because ItemPreview does not support this any more

--- src/sections/home/GameSection.tsx
+++ src/sections/home/GameSection.tsx
@@ -13,16 +13,16 @@ const GameSection = () => {
         <p>{language["game.description"]}</p>
         <Row>
             <Col xs={4} sm={4} md={3} lg={2}>
-                <ItemPreview name={language["game.ghosthouse.heading"]} image={"./magic_backpack-2.png"} rareness={"legendary"}/>
+                <ItemPreview image={"./magic_backpack-2.png"} rareness={"legendary"} type={"backpack"}/>
             </Col>
             <Col xs={4} sm={4} md={3} lg={2}>
-                <ItemPreview name={language["game.manhunt.heading"]} image={"./magic_backpack-2.png"} rareness={"common"}/>
+                <ItemPreview type={"backpack"} image={"./magic_backpack-2.png"} rareness={"common"}/>
             </Col>
             <Col xs={4} sm={4} md={3} lg={2}>
-                <ItemPreview tag={language["game.soon.heading"]} disabled={true} name={language["game.soon.heading"]} image={"./magic_backpack-2.png"} rareness={"common"}/>
+                <ItemPreview type={"backpack"} tag={language["game.soon.heading"]} disabled={true} image={"./magic_backpack-2.png"} rareness={"common"}/>
             </Col>
             <Col xs={4} sm={4} md={3} lg={2}>
-                <ItemPreview tag={language["game.soon.heading"]} disabled={true} name={language["game.soon.heading"]} image={"./magic_backpack-2.png"} rareness={"common"}/>
+                <ItemPreview type={"backpack"} tag={language["game.soon.heading"]} disabled={true} image={"./magic_backpack-2.png"} rareness={"common"}/>
             </Col>
         </Row>
     </Section>
