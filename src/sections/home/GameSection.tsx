a0b7d9bce0e23b22f476116d6b0029c28f777e54
nicosammito <nico@sammito.de>
2023-10-23 15:48:51 +0200

    fix: using News to visualize new games for the moment

--- src/sections/home/GameSection.tsx
+++ src/sections/home/GameSection.tsx
@@ -2,27 +2,48 @@ import {Col, Row} from "react-bootstrap";
 import Section from "../../micro-components/Section";
 import {useLanguage} from "../../hooks/LanguageHook";
 import {ItemPreview} from "../../micro-components/ItemPreview/ItemPreview";
+import News from "../../micro-components/News/News";
 
 const GameSection = () => {
 
     const language = useLanguage();
 
     return <Section>
-        <h1>{language["game.heading.1"]}</h1><br/>
-        <h1><span className="mark">{language["game.heading.2"]}</span></h1><br/>
-        <p>{language["game.description"]}</p>
         <Row>
-            <Col xs={4} sm={4} md={3} lg={2}>
-                <ItemPreview image={"./magic_backpack-2.png"} rareness={"legendary"} type={"backpack"}/>
+            <Col lg={7}>
+                <h1>{language["game.heading.1"]}</h1><br/>
+                <h1><span className="mark">{language["game.heading.2"]}</span></h1><br/>
+                <p>{language["game.description"]}</p>
             </Col>
-            <Col xs={4} sm={4} md={3} lg={2}>
-                <ItemPreview type={"backpack"} image={"./magic_backpack-2.png"} rareness={"common"}/>
-            </Col>
-            <Col xs={4} sm={4} md={3} lg={2}>
-                <ItemPreview type={"backpack"} tag={language["game.soon.heading"]} disabled={true} image={"./magic_backpack-2.png"} rareness={"common"}/>
-            </Col>
-            <Col xs={4} sm={4} md={3} lg={2}>
-                <ItemPreview type={"backpack"} tag={language["game.soon.heading"]} disabled={true} image={"./magic_backpack-2.png"} rareness={"common"}/>
+            <Col lg={5}>
+                <Row>
+                    <Col lg={6}>
+                        <News date={new Date()}
+                              title={"Uroria Update 2.0"}
+                              description={"Uroria is an event-hosting network, which allows creators to create an unique..."}
+                              image={"./assets/images/2.png"}/>
+                    </Col>
+                    <Col lg={6}>
+                        <News date={new Date()}
+                              title={"Uroria Update 2.0"}
+                              description={"Uroria is an event-hosting network, which allows creators to create an unique..."}
+                              image={"./assets/images/2.png"}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={6}>
+                        <News date={new Date()}
+                              title={"Uroria Update 2.0"}
+                              description={"Uroria is an event-hosting network, which allows creators to create an unique..."}
+                              image={"./assets/images/2.png"}/>
+                    </Col>
+                    <Col lg={6}>
+                        <News date={new Date()}
+                              title={"Uroria Update 2.0"}
+                              description={"Uroria is an event-hosting network, which allows creators to create an unique..."}
+                              image={"./assets/images/2.png"}/>
+                    </Col>
+                </Row>
             </Col>
         </Row>
     </Section>
