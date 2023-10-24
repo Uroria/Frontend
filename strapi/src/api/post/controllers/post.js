bdefda6a81d41f1184b154a642f2ddbcfbe905ef
nicosammito <nico@sammito.de>
2023-10-24 19:47:32 +0200

    new: own controller to search posts via slug and id instead of just id

--- strapi/src/api/post/controllers/post.js
+++ strapi/src/api/post/controllers/post.js
@@ -6,4 +6,23 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post',  ({ strapi }) =>  ({
+  async findOne(ctx) {
+
+    await this.validateQuery(ctx);
+    const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+
+    console.log(sanitizedQueryParams)
+
+    const {id} = ctx.params
+    // sanitizeQuery to remove any query params that are invalid or the user does not have access to
+    // It is strongly recommended to use sanitizeQuery even if validateQuery is used
+    const entity = await strapi.service('api::post.post').find({
+      where: { slug: id },
+      populate: sanitizedQueryParams.populate
+    });
+    const sanitizedResults = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedResults);
+  }
+}));
