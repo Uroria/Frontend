4df81f6e775172de7de98add4300fee75299b2b7
nicosammito <nico@sammito.de>
2023-11-07 17:55:00 +0100

    feat: BEM naming conv and fixed active styling

--- src/micro-components/Navigation/Navigation.tsx
+++ src/micro-components/Navigation/Navigation.tsx
@@ -2,11 +2,9 @@
 
 import {Container, Nav, Navbar} from "react-bootstrap";
 import styles from './Navigation.module.scss'
-import React, {FunctionComponent, useEffect, useRef, useState} from "react";
+import React from "react";
 import Link from "next/link";
 import Label from "../Label";
-import menu from "../../../public/assets/menu.json"
-import {Player} from "@lottiefiles/react-lottie-player";
 import Breadcrumb from "../Breadcrumb/Breadcrumb";
 import {IconHome2} from "@tabler/icons-react";
 import {usePathname} from "next/navigation";
@@ -18,11 +16,14 @@ const Navigation = () => {
 
     const t = useTranslations('Index');
     const locale = useLocale();
-
     const pathname: string = usePathname() || "";
+
+    //filter out current locale and pages with params in the url
     const breadcrumb: string[] = pathname.slice(1, pathname.length).split("/").filter(value => {
         return !(value.startsWith("[") && value.endsWith("]") || value.includes(locale))
     });
+
+    //create the matching link
     const breadcrumbLink = (index: number): string => {
         return breadcrumb.map((value, tindex) => {
             return index >= tindex ? `/${value}` : "";
@@ -30,9 +31,9 @@ const Navigation = () => {
     }
 
 
-    return <div className={styles["navigation"]}>
+    return <div className={styles["nav"]}>
         <Container style={{position: "relative"}}>
-            <Navbar collapseOnSelect expand="lg" className={styles["navigation__bar"]}>
+            <Navbar collapseOnSelect expand="lg" className={styles["nav__bar"]}>
                 <Navbar.Brand href="/">
                     <img
                         src="/assets/images/Uroria.svg"
@@ -44,28 +45,28 @@ const Navigation = () => {
                 <Navbar.Toggle style={{border: "none"}} aria-controls="responsive-navbar-nav">
                     <NavigationBurger/>
                 </Navbar.Toggle>
-                <Navbar.Collapse className={styles["nav-collapse"]} id="responsive-navbar-nav">
+                <Navbar.Collapse className={styles["nav__collapse"]} id="responsive-navbar-nav">
                     <Nav className="me-auto my-2 my-lg-0">
                     </Nav>
                     <Nav className="d-flex">
                         <div
-                            className={styles["navbar__nav-item"] + (pathname == getHrefWithoutLocale("/") ? " " + styles["navbar__nav-item-active"] : "")}>
+                            className={styles["nav__item"] + ("/" == getHrefWithoutLocale(pathname, locale) ? " " + styles["nav__item--active"] : "")}>
                             <Link href={"/"}>{t("nav-links-home")}</Link>
                         </div>
                         <div
-                            className={styles["navbar__nav-item"] + (pathname == getHrefWithoutLocale("/creator") ? " " + styles["navbar__nav-item-active"] : "")}>
+                            className={styles["nav__item"] + (getHrefWithoutLocale(pathname, locale).includes("/creator") ? " " + styles["nav__item--active"] : "")}>
                             <Link href={"#soon"}>{t("nav-links-creator")}</Link>
                             <Label onNavigation={true}>{t("nav-label-soon")}</Label>
                         </div>
                         <div
-                            className={styles["navbar__nav-item"] + (pathname == getHrefWithoutLocale("/shop") ? " " + styles["navbar__nav-item-active"] : "")}>
+                            className={styles["nav__item"] + (getHrefWithoutLocale(pathname, locale).includes("/shop") ? " " + styles["nav__item--active"] : "")}>
                             <Link href={"/shop"}>{t("nav-links-shop")}</Link>
                             <Label onNavigation={true}>{t("nav-label-soon")}</Label>
                         </div>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-            <div className={styles["navigation__breadcrumb-wrapper"]}>
+            <div className={styles["nav__breadcrumbs"]}>
 
                 {
                     //show home icon before all subpages and only when on subpage
@@ -87,43 +88,8 @@ const Navigation = () => {
 }
 
 
-const NavigationBurger: FunctionComponent = () => {
-
-    const playerref = useRef<Player>();
-    const [checked, setChecked] = useState<boolean>(false);
-
-    useEffect(() => {
-        if (checked) {
-            playerref.current?.setPlayerDirection(1);
-        } else {
-            playerref.current?.setPlayerDirection(-1);
-        }
-
-    }, [checked])
-
-    // @ts-ignore
-    return <Player ref={playerref}
-                   src={menu}
-                   className={styles["nav-menu"]}
-                   renderer={"svg"}
-                   keepLastFrame={true}
-                   speed={2}
-                   onEvent={event => {
-
-                       if (event != "load") return;
-
-                       playerref.current?.container?.addEventListener("click", () => {
-                           playerref.current?.play();
-                           setChecked(prevState => !prevState);
-                       })
-                   }}
-    />;
-}
-
-
-const getHrefWithoutLocale = (href = "/") => {
-    const locale = "en-US"
-    return locale && href.includes(locale) ? href.replace(locale, "") : href;
+const getHrefWithoutLocale = (href = "/", locale = "en-US") => {
+    return locale && href.includes(locale) ? href.replace(`${locale}`, "") : href;
 }
 
 export default Navigation;
