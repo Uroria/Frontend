68b5216a5ebaaf697a082efebc058fbd3e141b78
nicosammito <nico@sammito.de>
2023-11-12 01:08:43 +0100

    added more columns

--- src/macro-components/Posts/Posts.tsx
+++ src/macro-components/Posts/Posts.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {FunctionComponent} from "react";
 import Section from "../../micro-components/Section/Section";
 import News from "../../micro-components/News/News";
@@ -9,6 +7,15 @@ const Posts: FunctionComponent = () => {
   return <Section>
       <h2>Updates</h2>
       <Row>
+          <Col sm={12} lg={4}>
+              <News title={"Test"} description={"sdksdkssjs dsjdksksd"} date={new Date()} image={"/assets/images/1.png"}/>
+          </Col>
+          <Col sm={12} lg={4}>
+              <News title={"Test"} description={"sdksdkssjs dsjdksksd"} date={new Date()} image={"/assets/images/1.png"}/>
+          </Col>
+          <Col sm={12} lg={4}>
+              <News title={"Test"} description={"sdksdkssjs dsjdksksd"} date={new Date()} image={"/assets/images/1.png"}/>
+          </Col>
           <Col sm={12} lg={4}>
               <News title={"Test"} description={"sdksdkssjs dsjdksksd"} date={new Date()} image={"/assets/images/1.png"}/>
           </Col>
